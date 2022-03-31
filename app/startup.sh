ec52c975803bf19a44c333b0f5c5bbdef9452ea6
elbrus <elbrus.infosec@gmail.com>
2022-03-31 10:05:55 +0600

    added updates

--- app/startup.sh
+++ app/startup.sh
@@ -4,4 +4,4 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
-gunicorn app.wsgi:application --reload -w 2 -b :8000
+gunicorn app.wsgi:application --reload -w 2 -b 0.0.0.0:8000
